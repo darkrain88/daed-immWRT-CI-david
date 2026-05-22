69e1552308118f904396931e21c6880f7f185a3c
darkrain88 <39786175+darkrain88@users.noreply.github.com>
2026-05-22 22:24:11 +0800

    Update luci-app-daed package source

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -77,7 +77,7 @@ UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-ap
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
 
-UPDATE_PACKAGE "luci-app-daed" "breeze303/luci-app-daed" "kix"
+UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "kix"
 UPDATE_PACKAGE "luci-app-pushbot" "zzsj0928/luci-app-pushbot" "master"
 UPDATE_PACKAGE "luci-app-lucky" "sirpdboy/luci-app-lucky" "main"
 #更新软件包版本
