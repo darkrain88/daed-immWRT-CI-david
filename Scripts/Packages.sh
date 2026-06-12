7b7bd3f75c7ad99bee09a59ea6235ff6982221c3
darkrain88 <39786175+darkrain88@users.noreply.github.com>
2026-06-12 22:28:30 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -79,7 +79,8 @@ UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
 UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "kix"
 UPDATE_PACKAGE "luci-app-pushbot" "zzsj0928/luci-app-pushbot" "master"
-UPDATE_PACKAGE "luci-app-lucky" "sirpdboy/luci-app-lucky" "main"
+# UPDATE_PACKAGE "luci-app-lucky" "sirpdboy/luci-app-lucky" "main"
+UPDATE_PACKAGE "lucky" "gdy666/luci-app-lucky" "main"
 #更新软件包版本
 UPDATE_VERSION() {
 	local PKG_NAME=$1
